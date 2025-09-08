c4af135cfd8ea66245e77b628d1b148262766400
Daraindkkhan <darainkhanthesmartboi@gmail.com>
2025-09-08 17:45:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 13, 2017 18:03:26",
+	date: "September 10, 2025 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
